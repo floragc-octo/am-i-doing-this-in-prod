4504c9457c81fd424962b269fa529a33fbec5f01
Simon Belbeoch <simon.belbeoch@octo.com>
2021-02-06 11:47:27 +0100

    tech fix lint

--- extension/src/core/config.js
+++ extension/src/core/config.js
@@ -2,7 +2,7 @@ const defaultGetter = {
   config: [],
 }
 
-store.get(defaultGetter, ({ config }) => {
+chrome.storage.sync.get(defaultGetter, ({ config }) => {
   const event = new CustomEvent('plugin_loaded', { detail: config })
   document.dispatchEvent(event)
 })
