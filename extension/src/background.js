c669b63bb81f8a29f1c6b7431d5043ef2eb5b47b
Simon Belbeoch <simon.belbeoch@ln1.eu>
2025-11-23 16:07:49 +0100

    fix: fix ouverture automatique au clic sur firefox

--- extension/src/background.js
+++ extension/src/background.js
@@ -1,4 +1,12 @@
 // Open side panel when extension icon is clicked
+// Compatible with both Chrome and Firefox
 chrome.action.onClicked.addListener((tab) => {
-  chrome.sidePanel.open({ windowId: tab.windowId });
-});
+  // Check if we're on Firefox (which uses browser namespace)
+  if (typeof browser !== 'undefined' && browser.sidebarAction) {
+    // Firefox: open sidebar
+    browser.sidebarAction.open()
+  } else if (chrome.sidePanel) {
+    // Chrome/Edge: open side panel
+    chrome.sidePanel.open({ windowId: tab.windowId })
+  }
+})
